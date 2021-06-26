2632c363f6a28e1c3095bfaa4d234e9e451ca955
Ashvini shewale <83863564+AJshewale@users.noreply.github.com>
2021-06-26 16:41:21 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Foot from "./Foot";
+import Header from "./Header";
+import Infoo from "./Infoo";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Foot />
+      <Infoo />
+      <Infoo />
+      <Infoo />
+    </div>
+  );
+}
+
+export default App;
---
+++ Infoo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Infoo() {
+  return (
+    <div className="note">
+      <h1> Javascrpt and React.js</h1>
+      <p> A basic web dev React Js Bootcamp</p>
+    </div>
+  );
+}
+
+export default Infoo;
